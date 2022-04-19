4189d03c31d69da8fc24c3d1748d7926eb4de4a8
Rosonlau <paynerq@163.com>
2022-04-20 00:33:30 +0800

    Dockerfile without flask version

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from telegram.ext import *
 from bot import Bot
 import logging
-from flask import Flask
-import os
 
-app = Flask(__name__)
-
-@app.route("/")
 def main():
     # logging information
     
@@ -35,4 +30,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    main()
